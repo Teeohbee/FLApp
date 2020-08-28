acd53be23da31dbc7453b136a11b8224c99a35ca
Toby Clarke <teeohbee@gmail.com>
2020-08-28 10:58:22 +0100

    Display title and button to show character creation form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showCharacterCreationForm: false,
+    }
+    // this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit() {
+    this.setState(
+      { showCharacterCreationForm: true }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          FABLED LANDS
+        </header>
+        <section>
+          {
+            this.state.showCharacterCreationForm
+            ? <CreateCharacter />
+            : <form onSubmit={() => this.onSubmit()}>
+                <button>Create</button>
+              </form>
+          }
+        </section>
+      </div>
+    );
+  }
+}
+
+class CreateCharacter extends React.Component {
+  render() {
+    return (
+      <div>Hello I'm a character creation form!</div>
+    )
+  }
 }
 
 export default App;
